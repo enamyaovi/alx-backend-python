3771d7a68f62b0d16bfbd8f77ecf79d2eff00b77
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-07-06 12:07:48 +0000

    moved test(main) to correct directory

--- python-generators-0x00/1-main.py
+++ python-generators-0x00/1-main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from itertools import islice
-stream_users = __import__('0-stream_users_copy')
+
+stream_users = __import__('0-stream_users')
 
 # iterate over the generator function and print only the first 6 rows
 
--- python-generators-0x00/2-main.py
+++ python-generators-0x00/2-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-processing = __import__('1-batch_processing_copy')
+processing = __import__('1-batch_processing')
 
 ##### print processed users in a batch of 50
 try:
