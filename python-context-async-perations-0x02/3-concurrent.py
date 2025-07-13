74c9722b679eba6defef737c291f04ab0fb5b544
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-07-13 00:44:39 +0000

    Checker failed due to fnx name, corrected

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -3,7 +3,7 @@ import aiosqlite
 import asyncio
 
 
-async def async_fetch_all_users():
+async def async_fetch_users():
     async with aiosqlite.connect('users.db') as cnx:
         async with cnx.execute("SELECT * FROM users;") as cursor:
             row = await cursor.fetchall()
@@ -16,7 +16,7 @@ async def async_fetch_older_users():
             return row
 
 async def fetch_concurrently():
-    result = await asyncio.gather(async_fetch_all_users(), async_fetch_older_users())
+    result = await asyncio.gather(async_fetch_users(), async_fetch_older_users())
     return result
 
 if __name__ == "__main__":
