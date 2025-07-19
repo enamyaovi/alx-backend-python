6611fe2b9a293acbc258583bd338291d31d0d22c
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-07-19 19:00:08 +0000

    exposed token obtaining view

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,11 +2,11 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 import chats.views as views
 
-router = DefaultRouter()
-router.register(r'messages', views.MessagesViewSet, basename='messages')
-router.register(r'conversations', views.ConversationViewSet, basename='conversations')
+NestedDefaultRouter = DefaultRouter()
+NestedDefaultRouter.register(r'messages', views.MessagesViewSet, basename='messages')
+NestedDefaultRouter.register(r'conversations', views.ConversationViewSet, basename='conversations')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(NestedDefaultRouter.urls)),
     path('get_token/', views.get_token, name='get-token')
 ]
