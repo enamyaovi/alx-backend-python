e147ab39fb16d924e5d1e88f67931f3c79aeeb99
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-07-25 11:57:33 +0000

    Dummy settings file added

---
+++ Django-Middleware-0x03/settings.py
@@ -0,0 +1,16 @@
+
+#dummy ignore this
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware'
+]
