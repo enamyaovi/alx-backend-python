a68c7606944ddc229ee3495fb23b19112094ede8
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-07-30 15:20:37 +0000

    First draft of week 5 signals

---
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -0,0 +1,30 @@
+from django.db.models.signals import post_save, post_delete, pre_save
+from messaging.models import Message, Notifications, MessageHistory
+from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+
+@receiver(post_save, sender=Message)
+def notification_creator(sender, instance, created, **kwargs):
+    if created:
+        Notifications.objects.get_or_create(
+            user=instance.reciever,
+            message = instance
+        )
+
+@receiver(pre_save, sender=Message)
+def log_old_messages(sender, instance, **kwargs):
+    if instance.pk is not None:
+        old = Message.objects.filter(pk=instance.pk).first()
+        if old and old.content != instance.content:
+            instance.edited = True
+            MessageHistory.objects.create(
+                message=old,
+                old_content=old.content
+            )
+
+
+@receiver(post_delete, sender=get_user_model())
+def on_user_deleted(sender, instance, **kwargs):
+    MessageHistory.objects.filter(message__sender=instance).delete()
+    Message.objects.filter(sender=instance).delete()
+    Notifications.objects.filter(user=instance).delete()
---
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -0,0 +1,26 @@
+from rest_framework import routers
+from django.urls import path, include
+from messaging.views import (
+    MessageViewSet, ConversationViewSet, get_token,
+    UserViewSet, log_out, delete_user)
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+
+
+
+NestedDefaultRouter = routers.DefaultRouter()
+NestedDefaultRouter.register(r'messages', MessageViewSet, basename='messages')
+NestedDefaultRouter.register(r'conversations', ConversationViewSet, basename='conversations')
+NestedDefaultRouter.register(r'users', UserViewSet, basename='users')
+
+urlpatterns = [
+    path('', include(NestedDefaultRouter.urls)),
+    path('get_token/', get_token, name='get-token'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', log_out, name='log_out'),
+    path('user/delete_user/', delete_user, name='delete_user')
+]
