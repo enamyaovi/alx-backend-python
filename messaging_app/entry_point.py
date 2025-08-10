2601212f0dbd0e2b520741347d1f887bd0a700ff
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-08-10 00:52:24 +0000

    Dockerfile for development setup with an entry_point script

---
+++ messaging_app/entry_point.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+import sys
+import subprocess
+
+def main():
+    # Ensure Django knows which settings to use
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "messaging_app.settings")
+
+    subprocess.run(["python3", "manage.py", "makemigrations"], check=True)
+
+    # (Optional) Run migrations automatically
+    subprocess.run(["python", "manage.py", "migrate"], check=True)
+
+    # Start the development server
+    subprocess.run(["python", "manage.py", "runserver", "0.0.0.0:8000"], check=True)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
