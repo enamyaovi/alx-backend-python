359ea022bf6526763e42eca9dda91ea655367106
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-08-22 12:19:38 +0000

    submission for uncompleted work[skip ci]

--- messaging_app/chats/tests.py
+++ messaging_app/chats/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class SomeTest(TestCase):
+    def test_should_fail(self):
+        self.assertFalse(True)
+
+    def test_should_pass(self):
+        self.assertTrue(True)
